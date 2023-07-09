86a58e72a7dd33a535e4335d6d3564376893dfd9
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-09 13:09:10 +0530

    video11 useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MyFragment from './tutorials/video10/Fragment';
 import UseEffect from './tutorials/video9/UseEffect';
+import UseStateTutorial from './tutorials/video11/UseStateTutorial';
 
 function App() {
   return (
     <div>
       {/* <UseEffect /> */}
-      <MyFragment/>
+      {/* <MyFragment/> */}
+      <UseStateTutorial />
     </div>
   );
 }
